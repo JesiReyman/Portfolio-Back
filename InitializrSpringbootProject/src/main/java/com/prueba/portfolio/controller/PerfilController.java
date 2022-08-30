79507933e04e51ae0d161d4d71012c9935eb531d
Jesica Reyman <reyman.jesica@gmail.com>
2022-08-30 14:27:08 -0300

    DER

--- InitializrSpringbootProject/src/main/java/com/prueba/portfolio/controller/PerfilController.java
+++ InitializrSpringbootProject/src/main/java/com/prueba/portfolio/controller/PerfilController.java
@@ -30,8 +30,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://portfolio-9dcf2.web.app")
+@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "https://portfolio-9dcf2.web.app")
 @RequestMapping("/perfil")
 public class PerfilController {
     private final PerfilService perfilService;
